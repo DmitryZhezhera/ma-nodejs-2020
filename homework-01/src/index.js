f0da88fb296f5b0d5b6a909e069b865e3a81fe28
DmitryZhezhera <zhezhera.d@mail.ru>
2019-11-10 23:24:21 +0200

    test

--- homework-01/src/index.js
+++ homework-01/src/index.js
@@ -1,16 +1,15 @@
-const http      = require('http'),
-      nanoid    = require('nanoid'),
+const http = require('http');
+const nanoid = require('nanoid');
 
-      hostname  = '127.0.0.1',
-      port      = 3000,
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(nanoid());
+});
 
-      server    = http.createServer((req, res) => {
-
-                    res.statusCode  = 200;
-                    res.setHeader('Content-Type', 'text/plain');
-                    res.end(nanoid());
-
-                  });
-
-server.listen(port, hostname, () => console.log(`Server is running at http://${hostname}:${port}/`));
+server.listen(port, hostname, () =>
+  console.log(`Server is running at http://${hostname}:${port}/`),
+);
 console.log("TEST");
