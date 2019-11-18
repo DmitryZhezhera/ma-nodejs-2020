1ee54e0deb1e9287650c57663b0c0579707ab9c8
DmitryZhezhera <zhezhera.d@mail.ru>
2019-11-18 09:46:32 +0200

    rename added

--- homework-02/src/app.js
+++ homework-02/src/app.js
@@ -1,15 +1,11 @@
-// console.log('sum_', sum(1, 1, 1));
-// earth.toString();
-// promise.then((resolve) => console.log(resolve));
-
-const { task1 } = require('./task');
-const { task2 } = require('./task');
+const { task1: sum } = require('./task');
+const { task2: earth } = require('./task');
 const { task3 } = require('./task');
 
 const boot = async () => {
-  console.log(task1(1, 1, 1));
+  console.log(sum(1, 1, 1));
   await task3.then((resolve) => console.log(resolve));
-  await task2.toString();
+  await earth.toString();
 };
 
 boot();
