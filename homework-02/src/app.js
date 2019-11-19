b0d5a487a527878fff1d3141ba1e3ebd5c52a43c
DmitryZhezhera <zhezhera.d@mail.ru>
2019-11-19 12:58:32 +0200

    fix by comments

--- homework-02/src/app.js
+++ homework-02/src/app.js
@@ -2,7 +2,7 @@ const { Task1: sum, Task2: Earth, Task3 } = require('./task');
 
 const boot = async () => {
   console.log(sum);
-  await Task3.then((resolve) => console.log(resolve));
+  console.log(await Task3);
   Earth.toString();
 };
 
